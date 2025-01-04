b7e8098ba1ff2c6d6e1b6544670256059f36e729
Xenxlx <serenanesrallah@outlook.com>
2025-01-03 23:00:38 -0500

    Pegging Progress

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 The aim of this project is to implement a calculator that correctly calculates the points of a cribbage hand.
 
 **Future features**: Computer vision model to view a hand of cards via video capture
+
+## _Outline_
+
+On a loop: hand out hands, ask for discard before pegging, pegging calculator, hand calculator after pegging then menu displaying points and who's ahead
+- Player starts with crib
+- Computer randomly discards cards
--- main.py
+++ main.py
@@ -1,226 +1,227 @@
 '''
-This is my attempt at making a cribbage score calculator
-
+This is my attempt at making a cribbage game!
 '''
-from itertools import combinations
-# Can make these lists dictionaries mapped to their respective values (K = 13)
-rank_lst = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-suit_lst = ['H', 'D', 'S', 'C']
+import hand_calc
+from random import randint
+DISCARDS = 2
 
-# Card class (card1 = Card(rank, suit)), NOT USED CURRENTLY
-# I'll come back if I learn how to implement OOP in this program
-'''class Card:
-    def __init__(self, rank, suit):
-        self.rank = get_hand()
-        self.suit = suit
+class Deck:
+    cards = [[rank, suit] for suit in hand_calc.SUIT_LST for rank in hand_calc.RANK_LST]
+    left = len(cards) # Hardcode to 52 or not?
 
+    def __str__(self):
+        cards = ""
+        for i in range(len(self.cards)):
+            cards += f"{self.cards[i]}\n"
+        return f"{cards}"
 
-card1 = Card(7, 'Hearts')'''
-
-def get_cut():
-    while True:
-        rank = input("Enter the rank of the cut card: ").upper()
-        if rank in rank_lst:
-            if rank.isdigit():
-                rank = int(rank)
-            break
-        else:
-            print("Rank must be a number between 2-10 or A, J, Q or K.")
-
-    while True:
-            suit = input("Enter the suit of the cut card: ").upper()
-
-            if suit not in suit_lst:
-                print("Suit must be H, D, S or C.")
-            else:
-                break
-
-    return rank, suit
-
-def get_hand():
-    card_number = 1
-    hand = []
-
-    while len(hand) < 4:  # Assuming a hand of 4 cards, 4 player game
-        while True:  # Inputting rank
-            rank = input("Enter the rank of card " + str(card_number) + ": ").upper()
-
-            if rank in rank_lst:  # Checks if rank is valid input AND .isdigit()
-                if rank.isdigit():
-                    rank = int(rank)
-                break
-            else:
-                print("Rank must be a number between 2-10 or A, J, Q or K.")
-
-        while True:  # Inputting suit
-            suit = input("Enter the suit of card " + str(card_number) + ": ").upper()
-
-            if suit not in suit_lst:
-                print("Suit must be H, D, S or C.")
-            else:
-                break
-
-        hand.append([rank, suit])
-        card_number += 1
-
-    print(hand)
-    return hand
-
-
-'''
-# REFERENCE ONLY, DELETE AFTER SELF CONFIRMATION
-def get_hand():
-    hand = []
-    card_number = 1
-
-    # Assuming a hand of 4 cards, 4 player game
-    while len(hand) <= 4:
-        hand.append(get_cards(card_number))
-        card_number += 1
-
-    return hand
-'''
-
-def pair_calc(hand):
-    points = 0
-    for i in range(len(hand) - 1):
-        for j in range(i + 1, len(hand)):
-            if hand[j][0] == hand[i][0]:
-                points += 2
-                print(f"Pair of {hand[j][0]}'s!")
-    return points
-
-def fifteen_calc(hand):
-    points = 0
-    for r in range(2, 6):
-        for comb in combinations(hand, r):
-            print(comb)
-            if sum(card[0] for card in comb) == 15:
-                print('15 for 2!')
-                points += 2
-    return points
-
-def flush_calc(hand):
-    # Flush (4-5 same suit cards) calc FIX
-    suit_length = 1
-    suit_hand = sorted(hand, key=lambda card: card[1])  # Sorts by suit
-    print(f'Suit hand: {suit_hand}')
-
-    i = 0
-    while i < len(suit_hand) - 1:
-        if suit_hand[i][1] == suit_hand[i + 1][1]:
-            suit_length += 1
-        i += 1
+    @classmethod
+    def deal(cls):
+        cls.left -= 1
+        return cls.cards.pop(randint(0, cls.left - 1))
     
-    if suit_length > 3:
-        points += suit_length
-        print(f'Flush of {suit_length} cards!')
+    @classmethod
+    def reset(cls):
+        cards = [[rank, suit] for suit in hand_calc.SUIT_LST for rank in hand_calc.RANK_LST]
+        left = len(cards)
 
-    print(f'Suit length: {suit_length}')
-
-
-def calculator():
-    points = 0
-    cut_rank, cut_suit = get_cut()
-    hand = get_hand()
-    hand.append([cut_rank, cut_suit])
-
-    # points = cut_calc + pair_calc(hand) + run_calc + flush_calc + fifteens_calc // AT THE END
-
-    # Cut calculator
-    if cut_rank == 'J':
-        points += 1
+class Game:
+    crib = []
+    crib_points = 0
+    peg_sequence = [] # FOR KEEPING TRACK OF PEGGING, MIGHT NOT BE NEEDED
+    peg_count = 0
+    cut = None
     
-    for i in range(len(hand)):
-        if hand[i][0] == 'J' and hand[i][1] == cut_suit:
-            points += 1
-
-    # HAND PREPROCESSING - Face cards in order (for runs & fifteens)
-    for i in range(len(hand)):
-        if hand[i][0] == 'A':
-            hand[i][0] = 1
-
-        elif hand[i][0] == 'J':
-            hand[i][0] = 11
-
-        elif hand[i][0] == 'Q':
-            hand[i][0] = 12
-
-        elif hand[i][0] == 'K':
-            hand[i][0] = 13
-
-
-    # Sorting for runs calculator (sorts by suit + rank)
-    sorted_hand = sorted(hand)  # Sort by suit would be useful for Gin Rummy calculator
-
-    # Remove + track duplicates for runs
-    i = 0
-    duplicate_rank = {}
-
-    while i < len(sorted_hand) - 1:
-        if sorted_hand[i][0] == sorted_hand[i+1][0]:
-            if duplicate_rank.get(sorted_hand[i][0]) is not None:
-                duplicate_rank[sorted_hand[i][0]] += 1
-
-            else:
-                duplicate_rank[sorted_hand[i][0]] = 2
-
-            sorted_hand.remove(sorted_hand[i])
-
-        else:
-            i += 1
-
-    print(f'Final sorted hand : {sorted_hand}')
-    print(f'Duplicate_rank: {duplicate_rank}')
-
-    # Runs calculator
-    run_length = 1
-    i = 0
-    multiplier = 0
+    @classmethod
+    def print_crib(cls):
+        hand = ""
+        for i in range(len(cls.crib)):
+            hand += f"{i+1}: " + "".join(cls.crib[i]) + "  "
+        print(hand)
     
-    while i < len(sorted_hand) - 1:
-        if sorted_hand[i][0] + 1 == sorted_hand[i + 1][0]:
-            run_length += 1
+    @classmethod
+    def get_cut(cls):
+        cls.cut = Deck.deal()
 
-            if duplicate_rank.get(sorted_hand[i][0]) is not None:
-                multiplier += duplicate_rank[sorted_hand[i][0]]
+    @classmethod
+    def round_reset(cls):
+        cls.peg_sequence = []
+        cls.peg_count = 0
 
-            if run_length == len(sorted_hand):
-                if multiplier > 0:
-                    points += len(sorted_hand) * multiplier
-                    print(f'{multiplier} run of: {run_length}')
+class Player:
+    def make_hand(self, num_cards):
+        for i in range(num_cards):
+            self.hand.append(Deck.deal())
+
+    def __init__(self, name, num_cards):
+        self.name = name
+        self.hand = []
+        self.make_hand(num_cards)
+        self.peg_hand = self.hand
+        self.left = len(self.hand)
+        self.crib = True
+        self.points = 0
+        self.points_string = "|"
+    
+    def __sub__(self, other):
+        return self.points - other.points
+    
+    # Might not be useful?
+    def add_point(self):
+        self.points += 1
+        self.points_string = " " + self.points_string
+
+    def print_hand(self):
+        hand = ""
+        for i in range(len(self.hand)):
+            hand += f"{i+1}: " + "".join(self.hand[i]) + "  "
+        return hand
+    
+    def __str__(self):
+        return f"{self.name}| {self.print_hand()}"
+
+    # Returns INDEX of card
+    def ask_card(self):
+        print(f"{self.name}'s hand|  {self.print_hand()}")
+
+        while True:
+            card = input("Select a number corresponding to a card in your hand: ")
+            if card.isdigit():
+                card = int(card)
+                if (1 <= card <= len(self.hand)) == True:
+                    return card - 1
                 else:
-                    points += len(sorted_hand)
-                    print(f'Run of: {run_length}')
-                break
+                    print(f"Please enter a number between 1 and {len(self.hand)}")
+            else:
+                print("Please enter an integer value.")
 
-        else:   # When run is finished
-            if run_length > 2:
-                if multiplier > 0:
-                    points += run_length * multiplier
-                    print(f'{multiplier} run of: {run_length}')
-                else:
-                    points += run_length
-                    print(f'Run of: {run_length}')
-            run_length = 1
-            multiplier = 0
+    # Suggestion: replace self.left to track pegging hand length, not real hand length (which is 4 anyways)
+    # PLEASE CONSIDER SHORTLY, STRONG YES FROM ME ATM BUT NOT SURE
+    def crib_discard(self, num):
+        if self.name == "Computer":
+            print("computer discard test")
+            for i in range(num):
+                Game.crib.append(self.hand.pop(randint(0, self.left - 1)))
+                self.left -= 1
+        else:
+            for i in range(num):
+                card = self.ask_card()
+                Game.crib.append(self.hand.pop(card))
+                self.left -= 1
 
-        i += 1
+    # Checks if player can go
+    def check_go(self):
+        for card in self.peg_hand:
+            if card[0] + Game.peg_count <= 31:
+                return True
+        return False
 
-    # HAND PREPROCESSING - Convert face cards to value of 10
-    i = 0
-    for i in range(len(hand)):
-        if 10 < hand[i][0] < 14:
-            hand[i][0] = 10
-            print(hand[i][0])
+    # Double check if this works properly
+    # Doesn't keep track of peg hand length
+    # FIX SO THAT IT ENSURES YOU PICK A CARD THAT ADDS UP TO LESS THAN 31 IN PEG_SEQUENCE
+    def peg_discard(self):
+        if self.check_go() == False:
+            return False
 
         else:
-            hand[i][0] = int(hand[i][0])
+            if self.name == "Computer":
+                    card = self.peg_hand.pop(randint(0, self.left - 1))
+                    Game.peg_sequence.append(card)
+                    Game.peg_count += card_conv(card)
 
-    # ADD RUNS CALCULATOR FUNCTION THEN WE'RE GOLDEN
-    points += fifteen_calc(hand) + flush_calc(hand)
+            else:
+                card = self.ask_card()
+                Game.peg_count += card_conv(self.hand[card])
+                Game.peg_sequence.append(self.peg_hand.pop(card))
 
-    print(f'Points: {points}')
+def print_main_menu():
+    print("---------------------")
+    print("       Cribbage      ")
+    print("      2 players     ")
+    print("   Hand: Rank-Suit")
+    print("---------------------")
 
+def print_board(computer, player):
+    print(f"Computers points: {computer.points}")
+    print(f"Players points: {player.points}")
+    print("Computer:")
+    print("_________________________________________________________________________________________________________________________END")
+    print(f"{computer.points_string}")
+    print(f"Player:")
+    print(f"{player.points_string}")
+    print("_________________________________________________________________________________________________________________________END")
 
-calculator()
+def test_peg_board(string):
+    for i in range(121):
+        string = " " + string
+    print(string)
+
+# Converts a card to its face value
+def card_conv(card):
+    if card[0] == 'A':
+        return 1
+    elif card[0] in ['K', 'Q', 'J']:
+        return 10
+    else:
+        return int(card[0])
+
+def pegging(computer: Player, player: Player):
+    curr = player
+    other = computer
+
+    # Remember to reset peg_sequence after every pegging round (A: Done?)
+    while computer.left != 0 or player.left != 0:
+        # CHECK FOR PLAYER WITH A HAND OF 0 CARDS, FIX FUNCTION FOR THAT CASE
+        if curr.peg_discard() == False:
+            print(f"{other.name}| Point from the Go!")
+
+        if Game.peg_count == 15:
+            print(f"{curr.name}| Fifteen for 2!")
+            curr.points += 2
+
+        elif Game.peg_count == 31:
+            print(f"{curr.name}| Thirty-one for 2!")
+            curr.points += 2
+            Game.round_reset()
+
+    # Swap discard turn
+    curr = other
+    if curr.name == "Computer":
+        other = player
+    else:
+        other = computer
+
+        
+def game(computer, player):
+    print_main_menu()
+
+    # This part should be looped
+    player.crib_discard(DISCARDS)
+    computer.crib_discard(DISCARDS)
+    Game.print_crib()
+    Game.get_cut()
+    # *call pegging here
+
+p1 = Player("Tyler", 6)
+computer = Player("Computer", 6)
+game(computer, p1)
+
+'''
+# WORKS
+# Test unit for dealing cards + card count (deck class)
+deck_example = Deck()
+print(f"{deck_example}")
+print("-----------------------------")
+print(f"DEAL 1: {deck_example.deal()}. {deck_example.left}")
+print(f"{deck_example}")
+print("-----------------------------")
+print(f"DEAL 2: {deck_example.deal()}. {deck_example.left}")
+print(f"{deck_example}")
+
+# Test for individual player hand dealing
+p1 = Player("Tyler", 4)
+for i in range(len(p1.hand)):
+    print(p1.hand[i])
+
+print(f"{deck_example}")
+'''
