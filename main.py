9c9d0d7486721c9cf81fe81ab6b1acfa6aa31519
Xenxlx <serenanesrallah@outlook.com>
2025-06-18 00:56:09 -0400

    Cleaned up and worked on flow of pegging round. Fixed checking for #'s of a kind. Needs more work on flow and accuracy of point counting.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import hand_calc
 from random import randint
 DISCARDS = 2
 
+# I could've made a card class but hand_calc takes has card property constants
+
 class Deck:
     cards = [[rank, suit] for suit in hand_calc.SUIT_LST for rank in hand_calc.RANK_LST]
     left = len(cards)
@@ -30,14 +32,15 @@ class Deck:
 class Game:
     crib = []
     crib_points = 0
-    peg_sequence = [] # FOR KEEPING TRACK OF PEGGING, MIGHT NOT BE NEEDED
+    peg_sequence = []
     peg_count = 0
     cut = None
-    
+
     @classmethod
     def print_crib(cls):
         hand = ""
         for i in range(len(cls.crib)):
+            # unsure if it's printing correctly/logically
             hand += f"{i+1}: " + "".join(cls.crib[i]) + "  "
         print(hand)
     
@@ -53,7 +56,7 @@ class Game:
         print(f"Pegging sequence: {hand}")
 
     @classmethod
-    def round_reset(cls):
+    def peg_round_reset(cls):
         cls.peg_sequence = []
         cls.peg_count = 0
 
@@ -89,8 +92,8 @@ class Player:
     def __str__(self):
         return f"{self.name}| {self.print_hand()}"
 
-    # Returns INDEX of card
-    def ask_card(self):
+    # Returns INDEX of card chosen for pegging turn
+    def ask_card(self) -> int:
         print(f"{self.name}'s hand|  {self.print_hand()}")
 
         while True:
@@ -98,6 +101,7 @@ class Player:
             if card.isdigit():
                 card = int(card)
                 if (1 <= card <= len(self.hand)) == True:
+                    self.left -= 1
                     return card - 1
                 else:
                     print(f"Please enter a number between 1 and {len(self.hand)}")
@@ -105,10 +109,12 @@ class Player:
                 print("Please enter an integer value.")
 
     # Suggestion: replace self.left to track pegging hand length, not real hand length (which is 4 anyways)
-    # PLEASE CONSIDER SHORTLY, STRONG YES FROM ME ATM BUT NOT SURE
+    # STRONG YES FROM ME ATM BUT NOT SURE
     def crib_discard(self, num):
+        print("\nDiscard 2 cards for the crib.")
+        print("--------------------------------------------------------")
+
         if self.name == "Computer":
-            print("computer discard test")
             for i in range(num):
                 Game.crib.append(self.hand.pop(randint(0, self.left - 1)))
                 self.left -= 1
@@ -116,31 +122,29 @@ class Player:
             for i in range(num):
                 card = self.ask_card()
                 Game.crib.append(self.hand.pop(card))
-                self.left -= 1
 
     # Checks if player can go
-    def check_go(self):
+    def check_over_31(self):
         for card in self.peg_hand:
-            if card[0] + Game.peg_count <= 31:
+            if card_convert(card[0]) + Game.peg_count <= 31:
                 return True
         return False
 
     # Double check if this works properly
     # Doesn't keep track of peg hand length
     # FIX SO THAT IT ENSURES YOU PICK A CARD THAT ADDS UP TO LESS THAN 31 IN PEG_SEQUENCE
-    def peg_discard(self):
-        if self.check_go() == False:
+    def peg_discard(self) -> bool:
+        if self.check_over_31() == False:
             return False
-
         else:
             if self.name == "Computer":
                     card = self.peg_hand.pop(randint(0, self.left - 1))
                     Game.peg_sequence.append(card)
-                    Game.peg_count += card_conv(card)
+                    Game.peg_count += card_convert(card)
 
             else:
                 card = self.ask_card()
-                Game.peg_count += card_conv(self.hand[card])
+                Game.peg_count += card_convert(self.hand[card])
                 Game.peg_sequence.append(self.peg_hand.pop(card))
             return True
 
@@ -175,50 +179,84 @@ def card_convert(card):
     else:
         return int(card[0])
 
+# Could be a Game method. Easy refactor
+def check_of_a_kind_pegging(peg_sequence) -> int:
+    tally = 1
+    if not len(peg_sequence):
+        return
+    for i in range(1, len(peg_sequence)):
+        if peg_sequence[i][0] == peg_sequence[i-1][0]:
+            tally += 1
+    return tally
+
+
 def pegging(computer: Player, player: Player):
     curr = player
     other = computer
 
+    print("--------------------------------------------------------")
+    print("                       PEGGING                       ")
+    print("--------------------------------------------------------")
+
     # Remember to reset peg_sequence after every pegging round (A: Done?)
     while computer.left != 0 or player.left != 0:
-        # CHECK FOR PLAYER WITH A HAND OF 0 CARDS, FIX FUNCTION FOR THAT CASE
-        if curr.peg_discard() == False:
+        # Add logic to handle
+        Game.print_peg_sequence()
+        print(f"Peg count: {Game.peg_count}")
+        of_a_kind = 0 # counts how many pairs/of a kind
+
+        if not curr.peg_discard():
             print(f"{other.name}| Point from the Go!")
             other.add_points(1)
-        # Add logic to handle 
+            Game.peg_round_reset()
+        else:
+            # Checks for pairs
+            of_a_kind = check_of_a_kind_pegging(Game.peg_sequence)
+            """
+                    if Game.peg_sequence[-1][0] == Game.peg_sequence[-2][0]:
+                        if Game.peg_sequence[-2][0] == Game.peg_sequence[-3][0]:
+                            if Game.peg_sequence[-3][0] == Game.peg_sequence[-4][0]:
+                                print(f"{curr.name} | Four of a kind for twelve!")
+                                curr.add_points(12)
+                            else:
+                                print(f"{curr.name} | Three of a kind for six!")
+                                curr.add_points(6)
+                        else:
+                            print(f"{curr.name}| Pair for two!")
+                            curr.add_points(2)
+                """
+            if of_a_kind == 2:
+                print(f"{curr.name} | Pair for two!")
+                curr.add_points(2)
+            elif of_a_kind == 3:
+                print(f"{curr.name} | Three of a kind for six!")
+                curr.add_points(6)
+            elif of_a_kind == 4:
+                print(f"{curr.name} | Four of a kind for twelve!")
+                curr.add_points(12)
 
-        # Checks for pairs
-        if Game.peg_sequence[-1][0] == Game.peg_sequence[-2][0]:
-            print(f"{curr.name}| Pair for two!")
-            curr.add_points(2)
-            # Add pair status..? OR nested ifs
+            if Game.peg_count == 15:
+                print(f"{curr.name}| Fifteen for 2!")
+                curr.add_points(2)
 
-        # Check for 4 of same number
+            elif Game.peg_count == 31:
+                print(f"{curr.name}| Thirty-one for 2!")
+                curr.add_points(2)
+                Game.peg_round_reset()
 
-
-
-        if Game.peg_count == 15:
-            print(f"{curr.name}| Fifteen for 2!")
-            curr.add_points(2)
-
-        elif Game.peg_count == 31:
-            print(f"{curr.name}| Thirty-one for 2!")
-            curr.add_points(2)
-            Game.round_reset()
-
-    # Swap pegging turn
-    curr, other = other, curr
+            # Swap pegging turn
+        curr, other = other, curr
 
         
 def game(computer, player):
     print_main_menu()
 
-    # This part should be looped
+    # This part should be looped for entire 120 point game
     player.crib_discard(DISCARDS)
     computer.crib_discard(DISCARDS)
-    Game.print_crib()
     Game.get_cut()
-    # *call pegging here
+    pegging(computer, player)
+    Game.print_crib()
 
 p1 = Player("Tyler", 6)
 computer = Player("Computer", 6)
